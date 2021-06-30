1c863c12092b8a83cdd8ad6d3e3086dcc618c7ca
Raksha Nair <rakshanr5@gmail.com>
2021-06-30 23:21:05 +0530

    Part 2: Solution

---
+++ src/main/java/SelectedItems.java
@@ -0,0 +1,13 @@
+public class SelectedItems {
+
+    private String itemName;
+    private Integer itemPrice;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public Integer getItemPrice() {
+        return itemPrice;
+    }
+}
---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,98 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+    @BeforeEach
+    public void addRestaurant(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("23:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+    }
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+
+        Assertions.assertEquals(true,restaurant.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        Assertions.assertEquals(false, restaurant.isRestaurantOpen()); //Will fail when restaurant timings are out of bounds <10:30am to 11pm>
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+    @Test
+    public void when_no_items_are_added_to_the_cart(){
+
+        restaurant = Mockito.spy(restaurant);
+        List<Item> selectedItems = new ArrayList<>();
+        Assertions.assertEquals(0,restaurant.getTotalPriceOfOrder(selectedItems));
+        System.out.println("No items added to the cart");
+    }
+
+    @Test
+    public void when_only_one_item_is_added_to_the_cart(){
+
+        restaurant = Mockito.spy(restaurant);
+        Item i = new Item("Chole Bhature",100);
+        List<Item> selectedItems = new ArrayList<>();
+        selectedItems.add(i);
+        Assertions.assertEquals(100,restaurant.getTotalPriceOfOrder(selectedItems));
+        System.out.println("Your total order will cost: Rs."+restaurant.getTotalPriceOfOrder(selectedItems));
+    }
+
+    @Test
+    public void when_two_items_are_added_to_the_cart(){
+
+        restaurant = Mockito.spy(restaurant);
+        Item i1 = new Item("Chole Bhature",100);
+        Item i2 = new Item("Raj Kachori",120);
+        List<Item> selectedItems = new ArrayList<>();
+        selectedItems.add(i1);
+        selectedItems.add(i2);
+        Assertions.assertEquals(220,restaurant.getTotalPriceOfOrder(selectedItems));
+        System.out.println("Your total order will cost: Rs."+restaurant.getTotalPriceOfOrder(selectedItems));
+    }
+}
